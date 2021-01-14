4b1db7abaa46f7b962825495a3b8b2d1238350ed
Godreigneth <godreignethlevidibia@yahoo.com>
2021-01-14 23:38:36 +0000

    Created the js folder with the script.js file, and then declated the document ready function, then created an array with the 12 sets of image cards

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+    //Created an array with the 12 sets of cards
+  let img = [
+    'assets/images/1_Stockholm.jpg',
+    'assets/images/2_Gothenburg.jpg',
+    'assets/images/3_Malmo.jpg',
+    'assets/images/4_Uppsala.jpg',
+    'assets/images/5_Upplands_Vasby.jpg',
+    'assets/images/6_Vasteras.jpg',
+    'assets/images/7_Orebro.jpg',
+    'assets/images/8_Linkoping.jpg',
+    'assets/images/9_Helsingborg.jpg',
+    'assets/images/10_Jonkoping.jpg',
+    'assets/images/11_Norrkoping.jpg',
+    'assets/images/12_Lund.jpg'
+  ]
+
+
+});
