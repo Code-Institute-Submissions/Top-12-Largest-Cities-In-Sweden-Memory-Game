0b34fbe4b66d609d4710bf76dc99c72ddb6146f6
Godreigneth <godreignethlevidibia@yahoo.com>
2021-01-15 09:10:58 +0000

    Decleared the default move

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,27 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     //Created an array with the 12 sets of cards
-  let img = [
-    'assets/images/1_Stockholm.jpg',
-    'assets/images/2_Gothenburg.jpg',
-    'assets/images/3_Malmo.jpg',
-    'assets/images/4_Uppsala.jpg',
-    'assets/images/5_Upplands_Vasby.jpg',
-    'assets/images/6_Vasteras.jpg',
-    'assets/images/7_Orebro.jpg',
-    'assets/images/8_Linkoping.jpg',
-    'assets/images/9_Helsingborg.jpg',
-    'assets/images/10_Jonkoping.jpg',
-    'assets/images/11_Norrkoping.jpg',
-    'assets/images/12_Lund.jpg'
-  ]
+    let img = [
+        'assets/images/1_Stockholm.jpg',
+        'assets/images/2_Gothenburg.jpg',
+        'assets/images/3_Malmo.jpg',
+        'assets/images/4_Uppsala.jpg',
+        'assets/images/5_Upplands_Vasby.jpg',
+        'assets/images/6_Vasteras.jpg',
+        'assets/images/7_Orebro.jpg',
+        'assets/images/8_Linkoping.jpg',
+        'assets/images/9_Helsingborg.jpg',
+        'assets/images/10_Jonkoping.jpg',
+        'assets/images/11_Norrkoping.jpg',
+        'assets/images/12_Lund.jpg'
+    ]
 
+
+    //Decleared the default move
+    let total = 0,
+        move = 0,
+        count = 1,
+        first_card = null,
+        secn_card = null;
 
 });
